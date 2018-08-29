4358ac74e4d23c2b0959be8916f99bfb7cb0cd72
lushuhao <shahramlu@163.com>
2018-08-29 21:50:34 +0800

    重新引用插件

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,52 @@
 const gulp = require('gulp')
+const del = require('del')
 const fs = require('fs')
 const rename = require('gulp-rename')
+
+const through = require('through2')
+const colors = require('ansi-colors')
+const log = require('fancy-log')
+const argv = require('minimist')(process.argv.slice(2))
+
 const postcss = require('gulp-postcss')
 const px2rpx = require('postcss-px2rpx')
 const base64 = require('postcss-font-base64')
+
 const sass = require('gulp-sass')
 const combiner = require('stream-combiner2')
 const sourcemaps = require('gulp-sourcemaps')
 const runSequence = require('run-sequence')
-const del = require('del')
 const jdists = require('gulp-jdists')
-const through = require('through2')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
-const minifyCSS = require('gulp-minify-css')
-const minifyHtml = require('gulp-html-minify')
-const minifyJson = require('gulp-json-minify')
-const argv = require('minimist')(process.argv.slice(2))
+const htmlmini = require('gulp-htmlmin')
+const jsonminify = require('gulp-jsonminify')
+const cssnano = require('gulp-cssnano')
 
 // 判断gulp --type prod 命名 type 是否是生产打包
 const isProd = argv.type === 'prod'
 const src = './src'
 const dist = './dist'
 
+const handleError = (err) => {
+  console.log('\n')
+  log(colors.red('Error!'))
+  log('fileName: ' + colors.red(err.fileName))
+  log('lineNumber: ' + colors.red(err.lineNumber))
+  log('message: ' + err.message)
+  log('plugin: ' + colors.yellow(err.plugin))
+}
+
 gulp.task('wxml', () => {
   return gulp
     .src(`${src}/**/*.wxml`)
     .pipe(
-      isProd ? minifyHtml() : through.obj()
+      isProd ? htmlmini({
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        keepClosingSlash: true, //保持斜杠作为结尾
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+      }) : through.obj()
     )
     .pipe(gulp.dest(dist))
 })
@@ -35,21 +54,21 @@ gulp.task('wxml', () => {
 gulp.task('wxss', () => {
   const combined = combiner.obj([
     gulp.src(`${src}/**/*.{wxss,scss}`),
-    sass().on('error', sass.logError),
+    sass(),
     postcss([px2rpx(), base64()]),
     rename((path) => (path.extname = '.wxss')),
-    isProd ? minifyCSS() : through.obj(),
+    isProd ? cssnano() : through.obj(),
     gulp.dest(dist)
   ])
 
-  combined.on('error', console.error.bind(console))
+  combined.on('error', handleError)
 })
 
 gulp.task('js', () => {
   const combined = combiner.obj([
     gulp.src(`${src}/**/*.js`),
 
-    jdists({ trigger: isProd ? 'prod' : 'dev' }),
+    jdists({trigger: isProd ? 'prod' : 'dev'}),
 
     // 生产环境不生成shoucemap，传入空的流处理方法
     isProd ? through.obj() : sourcemaps.init(),
@@ -65,13 +84,13 @@ gulp.task('js', () => {
     gulp.dest(dist)
   ])
 
-  combined.on('error', console.error.bind(console))
+  combined.on('error', handleError)
 })
 
 gulp.task('json', () => {
   return gulp
     .src(`${src}/**/*.json`)
-    .pipe(isProd ? minifyJson() : through.obj())
+    .pipe(isProd ? jsonminify() : through.obj())
     .pipe(gulp.dest(dist))
 })
 
@@ -122,16 +141,16 @@ gulp.task('create', () => {
 
 const createWxFileList = (pathname, name) => {
   let jsContent, jsonContent
-   switch (pathname){
-     case 'components':
-       jsContent = 'Component({})'
-       jsonContent = '{\n  "component": true\n}'
-       break
-     case 'pages':
-       jsContent = 'Page({})'
-       jsonContent = `{\n  "navigationBarTitleText": "${name}"\n}`
-       break
-   }
+  switch (pathname) {
+    case 'components':
+      jsContent = 'Component({})'
+      jsonContent = '{\n  "component": true\n}'
+      break
+    case 'pages':
+      jsContent = 'Page({})'
+      jsonContent = `{\n  "navigationBarTitleText": "${name}"\n}`
+      break
+  }
   return [
     {
       ext: '.js',
