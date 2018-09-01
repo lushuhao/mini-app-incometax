97011e7d50ce8371eeae081e19d635898bb6a29b
lushuhao <shahramlu@163.com>
2018-09-02 03:27:42 +0800

    添加切换城市页面，主要处理缩略图和列表之间的联动

---
+++ src/pages/social/cityList.js
@@ -0,0 +1,130 @@
+import cityList from '../../collect/cityList'
+
+Page({
+  data: {
+    cityList: [],
+    cityKeyList: [], // 城市首字母列表
+    scrollTop: 0,
+    toCityKey: 'a', // 滚动位置的城市首字母
+    showToast: false,
+    touch: false, // touch事件触发
+  },
+  onLoad() {
+    this.setData({
+      cityList,
+      cityKeyList: Object.keys(cityList)
+    }, () => {
+      this.getShortCutOffset()
+    })
+    const self = this
+    wx.getSystemInfo({
+      success(res) {
+        self.windowHeight = res.windowHeight
+      }
+    })
+  },
+  onReady() {
+    // Do something when page ready.
+  },
+  onShow() {
+    // Do something when page show.
+  },
+  onHide() {
+    // Do something when page hide.
+  },
+  onUnload() {
+    // Do something when page close.
+  },
+  onShareAppMessage() {
+    // return custom share data when user share.
+  },
+  getShortCutOffset() {
+    const self = this
+    wx.createSelectorQuery().selectAll('.item').boundingClientRect(function (rect) {
+      self.targetList = rect
+    }).exec()
+    wx.createSelectorQuery().selectAll('.cityKey').boundingClientRect(function (rect) {
+      self.viewTargetList = rect
+    }).exec()
+  },
+  /**
+   * 滚动事件
+   * @description touch事件触发时，跳过
+   * @param e
+   */
+  bindScroll(e) {
+    if (this.data.touch) return
+    const scrollY = e.detail.scrollTop
+    this.viewTargetList.forEach((item, index) => {
+      const {top, bottom} = item
+      /**
+       * 可视外区域跳过循环
+       * target不变跳过循环
+       */
+      if (top - this.windowHeight / 2 > scrollY ||
+        bottom + this.windowHeight / 2 < scrollY ||
+        this.target && this.target.id === item.id) {
+        return
+      }
+      /**
+       * 三个首字母所在行[lastTop, lastBottom],[top，bottom],[nextTop, nextBottom]
+       * 滚动到一定区域认为已滚动到item
+       * 上方区域， [lastBottom, top]
+       * 下方区域， [bottom, nextTop]
+       */
+      const lastIndex = index <= 0 ? 0 : index - 1
+      const last = this.viewTargetList[lastIndex]
+      const nextIndex = index >= this.viewTargetList.length - 1 ? this.viewTargetList.length - 1 : index + 1
+      const next = this.viewTargetList[nextIndex]
+      const lastY = top - (top - last.bottom) / 2
+      const nextY = bottom + (next.top - bottom) / 2
+      if (lastY < scrollY && nextY > scrollY) {
+        console.log(top, lastY, scrollY, nextY, bottom)
+        this.target = item
+      }
+    })
+    this.target &&
+    this.target.id !== this.data.toCityKey &&
+    this.setData({
+      showToast: false,
+      toCityKey: this.target.id
+    })
+  },
+  touchStart(e) {
+    this.setData({
+      showToast: true
+    })
+    this.calculateTouchTarget(e.touches)
+  },
+  touchMove(e) {
+    this.calculateTouchTarget(e.touches)
+  },
+  touchEnd(e) {
+    this.calculateTouchTarget(e.changedTouches, false)
+  },
+  calculateTouchTarget(touches, touch = true) {
+    const {clientY} = touches[0]
+    const target = this.targetList.find(y => {
+      return y.top < clientY && y.bottom > clientY
+    })
+    this.toView(target)
+      .then(() => {
+        this.setData({touch})
+      })
+  },
+  toView(target) {
+    return new Promise(resolve => {
+      const {key} = target && target.dataset || ''
+      if (key &&
+        key !== this.data.toCityKey) {
+        const toTarget = this.viewTargetList.find(item => {
+          return item.id === key
+        })
+        this.setData({
+          scrollTop: toTarget.top,
+          toCityKey: key
+        }, resolve)
+      }
+    })
+  }
+})
